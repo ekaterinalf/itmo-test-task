e06c7e8f2666a1a578ee41348fef06a1d5fa44c7
Katerina <ekaterinalf@mail.ru>
2022-06-15 11:42:37 +0300

    add gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "245e4b1c616ea0c6"; }
+/******/ 		__webpack_require__.h = function() { return "927ab771b6443966"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.7a0d9a0c05e604a8.hot-update.js
+++ .next/static/webpack/webpack.7a0d9a0c05e604a8.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "d3119c89770cae08"; }
+/******/ 	__webpack_require__.h = function() { return "927ab771b6443966"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
